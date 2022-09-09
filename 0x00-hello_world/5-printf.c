3f47c4667ea0d52df739444545df5e297e7db4ce
Actoroffaith2 <richardson4divinity57@gmail.com>
2022-09-08 19:32:52 -0700

    program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,14 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-  
-
- /** 
-  *
-  *    * main - Entry point 
-  *
-  *       * 
-  *
-  *          * Return: 0 on success 
-  *
-  *             */ 
-
- int main(void) 
-
-	 { 
-
-		          printf("with proper grammar, but the outcome is a piece of art,\n"); 
-
-			           return (0); 
-
-				    }
+/**
+ * main - Entry point of programme
+ * @void: This function takes no arguments
+ *
+ * Description: Prints out text to standard output
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
