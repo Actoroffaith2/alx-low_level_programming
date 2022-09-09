31c022dc33655930bb80835a29ff83e59d088cc2
Actoroffaith2 <richardson4divinity57@gmail.com>
2022-09-08 18:58:03 -0700

    Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-/**
- * main - Printing
- *
- * Return Always 0 on Success
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle") ;
-	return (0);
-}
+  
+
+ /** 
+  *
+  *    * main - Entry point 
+  *
+  *       * 
+  *
+  *          * Return: 0 on success 
+  *
+  *             */ 
+
+ int main(void) 
+
+	 { 
+
+		          puts("\"Programming is like building a multilingual puzzle"); 
+
+			           return (0); 
+
+				    }
