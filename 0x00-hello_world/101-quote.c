0d1bc31d093847c94781d641c9c494efc9703917
Actoroffaith2 <richardson4divinity57@gmail.com>
2022-09-08 20:10:32 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point of programme
- * @void: This function has no parameters
+
+/** 
+ * main - Entry point
  *
- * Description: Prints out to the standard error
- * Return: 0 if successful
+ * Return: 1 on success
  */
-int main(void)
-{        char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	 write(2, buf, 59);
-	 return (1);
+int main(void) 
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
