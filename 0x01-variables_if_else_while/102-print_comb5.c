f3809e29d888da4fc4a83c7429183f92b5eee108
Actoroffaith2 <richardson4divinity57@gmail.com>
2022-09-09 20:49:36 -0700

    Write a program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,94 @@
 #include <stdio.h>
 
+
+
 /**
+
  * main - print pairs of double digit combos
+
  *
+
  * Description: print pairs of double digit combos
+
  *
+
  * Return: Always 0 (Success)
+
  */
 
+
+
 int main(void)
+
 {
+
 	int i, j, k, m;
 
+
+
 	i = 48;
+
+	while (i < 58)
+
 	{
+
 		j = 48;
+
 		while (j < 58)
+
 		{
-			putchar(i);
-			putchar(j);
-			putchar(32);
-			putchar(k);
-			putchar(m);
-			if (i < 57 || j < 56 || k < 57 || m < 57)
+
+			m = j + 1;
+
+			k = i;
+
+			while (k < 58)
+
 			{
-				putchar(44);
-				putchar(32);
+
+				while (m < 58)
+
+				{
+
+					putchar(i);
+
+					putchar(j);
+
+					putchar(32);
+
+					putchar(k);
+
+					putchar(m);
+
+					if (i < 57 || j < 56 || k < 57 || m < 57)
+
+					{
+
+						putchar(44);
+
+						putchar(32);
+
+					}
+
+					m++;
+
+				}
+
+				m = 48;
+
+				k++;
+
 			}
-			m++;
+
+			j++;
+
 		}
-		m = 48;
-		k++;
+
+		i++;
+
 	}
-	j++;
-}
-i++;
-}putchar(10);
-return (0);
-}
+
+	putchar(10);
+
+	return (0);
+
